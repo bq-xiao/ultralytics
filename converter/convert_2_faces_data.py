d380ff66a53fa763f3e9ba326dc9e182b7000ebb
肖保强 <xiaobq@shuhaisc.com>
2024-12-24 09:19:44 +0800

    add demo

---
+++ converter/convert_2_faces_data.py
@@ -0,0 +1,33 @@
+import os
+
+import pandas as pd
+
+csv = pd.read_csv('../../datasets/human-faces-object-detection/faces.csv')
+
+
+def convert_box(df):
+    width, height, x0, y0, x1, y1 = float(df['width']), float(df['height']), float(df['x0']), float(df['y0']), float(
+        df['x1']), float(df['y1'])
+
+    b_width = x1 - x0
+    b_heght = y1 - y0
+    x_center = b_width / 2
+    y_center = b_heght / 2
+    return round(x_center / width, 6), round(y_center / height, 6), \
+           round(b_width / width, 6), round(b_heght / height, 6)
+
+
+for index, row in csv.iterrows():
+    img_name = row['image_name']
+    file_array = img_name.split(".")
+    new_file = '../../datasets/human-faces-object-detection/labels/' + file_array[0] + ".txt"
+    x_center, y_center, w, h = convert_box(row)
+    line = "0 " + str(x_center) + " " + str(y_center) + " " + str(w) + " " + str(h) + "\n"
+    if os.path.exists(new_file):
+        with open(new_file, "a") as file:
+            file.write(line)
+            file.close()
+    else:
+        yolo_file = open(new_file, "w", encoding='utf-8')
+        yolo_file.write(line)
+        yolo_file.close()
--- sam_predictor.py
+++ sam_predictor.py
@@ -1,8 +1,9 @@
 from ultralytics import SAM
 
 # Load a model
-sam = SAM("sam2.1_l.pt")
+sam = SAM("sam2.1_b.pt")
 # Run inference with bboxes prompt
-segs = sam.predict(source="ultralytics/assets/zidane.jpg")
+segs = sam.predict(source="../datasets/human-faces-object-detection/images/00002798.jpg",
+                   bboxes=[106, 332, 264, 454], stream=False)
 for seg in segs:
-    seg.save(filename="111.jpg")
+    seg.save(filename="00002798.jpg")
